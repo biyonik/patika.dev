6aad26394ee28319c1ed78885f6b75441be5700a
ahmet <ahmet.altun60@gmail.com>
2022-03-21 22:42:49 +0300

    IfElseTernary Eklendi

---
+++ Net_Core_Patikasi/IfElseTernary/Program.cs
@@ -0,0 +1,11 @@
+﻿int time = DateTime.Now.Hour;
+
+if(time >= 6 && time < 11)
+    System.Console.WriteLine("Günaydın");
+else if(time >= 11 && time <= 18)
+    System.Console.WriteLine("İyi günler.");
+else
+    System.Console.WriteLine("İyi geceler.");
+
+string sonuc = time >= 6 && time < 11 ? "Günaydın" : time >= 11 && time <= 18 ? "İyi günler" : "İyi geceler";
+System.Console.WriteLine("Sonuç: "+sonuc);
