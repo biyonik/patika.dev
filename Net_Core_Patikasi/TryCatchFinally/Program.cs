4527c3b3b413075e73a02217bb2f3ce25231bcb7
ahmet <ahmet.altun60@gmail.com>
2022-03-21 22:03:33 +0300

    TryCatchFinally eklendi

---
+++ Net_Core_Patikasi/TryCatchFinally/Program.cs
@@ -0,0 +1,28 @@
+﻿/**
+*
+* Try - Catch - Finally
+*
+*/
+
+try
+{
+    System.Console.WriteLine("Bir sayı giriniz: ");
+    int girilenSayi = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine($"Girmiş olduğunuz sayı: {girilenSayi}");
+}
+catch (FormatException ex)
+{
+    System.Console.WriteLine(ex.Message);
+}
+finally {
+    System.Console.WriteLine("İşlem sonlandı....");
+}
+
+try {
+    // int a = int.Parse(null);
+    int a = int.Parse("Test");
+} catch(ArgumentNullException argumentNullEx) {
+    System.Console.WriteLine(argumentNullEx.Message);
+} catch(FormatException formatEx) {
+    System.Console.WriteLine(formatEx.Message);
+}
